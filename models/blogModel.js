71278b5993d170142b05d8c85c8cccbef81ea13c
Ankur Singh <ankursingh9806@gmail.com>
2024-09-07 00:03:33 +0530

    blog model - id, title, content, publlishetAt

---
+++ models/blogModel.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/database');
+
+const Blog = sequelize.define('Blog', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    publishedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+});
+
+module.exports = Blog;
