9c6548e83435ecdaeb2d10bc3aeee68b576ea5b2
Ankur Singh <ankursingh9806@gmail.com>
2024-09-07 00:20:23 +0530

    welcome

---
+++ public/js/welcome.js
@@ -0,0 +1,14 @@
+document.querySelector('.signup-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href = '../html/signup.html';
+});
+
+document.querySelector('.login-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href = '../html/login.html';
+});
+
+document.querySelector('.second-signup-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href = '../html/signup.html';
+});
